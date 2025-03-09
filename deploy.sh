79c469fa92c355e799ad6b11e7eacfc3ed2e52a5
tangerine238 <stephentang238@gmail.com>
2025-03-09 18:19:28 -0400

    adding stuff

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+set -e
+npm run build
+cd dist
+echo > .nojekyll
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+git push -f  git@github.com:tangerine238/ReactWebsite.git main:gh-pages
+
+cd - 
