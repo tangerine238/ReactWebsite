501fb1e144b82270b68bf8aa0fd38590edf38aaa
tangerine238 <stephentang238@gmail.com>
2025-03-17 16:17:56 -0400

    asdas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import styles from './App.module.css'
 import { About } from './components/About/About';
 import { Hero } from './components/Hero/Hero';
-import { Navbar } from './components/Navbar/Navbar';
+import { Navbar } from './components/navbar/navbar';
 import { Experience } from './components/Experience/Experience';
 import { Projects } from './components/Projects/Projects';
 import { Contact } from './components/Contact/Contact';
